050861112da0695bdc154f200e6ebb9563dc6893
Justin Taylor <kiradez211@gmail.com>
2023-12-23 16:18:02 -0500

    new update idk

---
+++ MainCharacter.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var moving: AnimationPlayer = $Mikey/AnimationPlayer
+@onready var visuals = $visuals
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		visuals.look_at(position + direction)
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	#$visuals
+		#moving.play("ArmatureAction")
+	
+	move_and_slide()
