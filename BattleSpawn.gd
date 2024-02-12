07ae7e3d417aacb1b36a635196f37a5032b65039
DarknessGear <nyeh211@gmail.com>
2024-02-12 18:11:14 -0500

    commented animation

--- BattleSpawn.gd
+++ BattleSpawn.gd
@@ -5,9 +5,9 @@ extends Area3D
 
 func _on_body_entered(body):
 	if "Sophia" in body.name:
-		$"../UI/AnimationPlayer".play("TransIn")
+		#$"../UI/AnimationPlayer".play("TransIn")
 		get_tree().paused = true
 		#await get_tree().create_timer(1.5).timeout
 		get_tree().change_scene_to_file("res://Battle Scene/Battle.tscn")
 		queue_free()
-		$"../UI/AnimationPlayer".play("TransOut")
+		#$"../UI/AnimationPlayer".play("TransOut")
